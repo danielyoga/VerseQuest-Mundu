055a0f4767322bbf4bf627782be8ebc52edc8fa0
Daniel Yoga <danielyoga@users.noreply.github.com>
2026-03-24 18:15:08 +0700

    Remove allowedDevOrigins from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,6 @@ const nextConfig: NextConfig = {
    * on your LAN (e.g. http://192.168.247.77:3000). Add more IPs here if your machine
    * gets a different address. Restart `npm run dev` after changing this.
    */
-  allowedDevOrigins: ["192.168.247.77"],
 };
 
 export default nextConfig;
